ecbbf58158cce0e1960b95564a8d70f8ca528562
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-07-18 15:18:40 +0300

    final?

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,30 +1,26 @@
 import axios from 'axios';
 
-
 const API_KEY =
   'live_var9FwMowJEaIygzwPYWcpnuB3xJoogqQd9JhcY4sxxCgiX7aTlNGUtXcca4YMno';
 
 axios.defaults.headers.common['x-api-key'] = API_KEY;
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1'
-
-
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
-  return axios(`/breeds`)
-    .then(resp => {  
-      return resp.data;
-    })
-    
+  return axios(`/breeds`).then(resp => {
+    return resp.data;
+  })
+  .catch(err => {
+    console.log(err);
+  })
 }
 
 export function fetchCatByBreed(breedId) {
   const FULL_URL = '/images/search?breed_ids=';
-
-  return axios(`${FULL_URL}${breedId}&api_key=${API_KEY}`)
-    .then(catData => {
-      return catData.data;
-    })
-    
+  return axios(`${FULL_URL}${breedId}&api_key=${API_KEY}`).then(catData => {
+    return catData.data;
+  })
+  .catch(err => {
+    console.log(err);
+  })
 }
-
-
--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ import { fetchBreeds } from './cat-api.js';
 import { fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.querySelector('select.breed-select');
-const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('p.loader');
+const error = document.querySelector('p.error');
 const divCatInfo = document.querySelector('.cat-info');
 
+error.hidden = true;
 breedSelect.hidden = true;
 
-let storeBreeds = [];
-
 fetchBreeds().then(resp => {
-  storeBreeds = resp;
-  let selectElement = document.querySelector('.breed-select');
-  storeBreeds.forEach(element => {
+  if (resp === undefined) {
+    return fault();
+  }
+  let customOption = document.createElement('option');
+  customOption.text = 'Select cat';
+  breedSelect.add(customOption);
+
+  resp.forEach(element => {
     let newOpt = document.createElement('option');
     newOpt.value = element.id;
     newOpt.text = element.name;
-    selectElement.add(newOpt);
-
+    breedSelect.add(newOpt);
     loader.hidden = true;
     breedSelect.hidden = false;
   });
@@ -27,14 +30,20 @@ fetchBreeds().then(resp => {
 breedSelect.addEventListener('change', () => {
   loader.hidden = false;
   divCatInfo.hidden = true;
+  breedSelect.remove(0);
 
   const breedId = breedSelect.value;
-  
+
   fetchCatByBreed(breedId).then(data => {
+    console.log(data);
+    if (data === undefined || data.length === 0) {
+      return fault();
+    }
+    console.log(data);
     let dataInfoCat = data[0].breeds[0];
     let { name, description, temperament } = dataInfoCat;
-    
-    catInfo.innerHTML = `
+
+    divCatInfo.innerHTML = `
      <div class="div-wrap">
      <img class="img" src="${data[0].url}" alt="${name}" width="600" >
      <div class="info-div">
@@ -44,9 +53,17 @@ breedSelect.addEventListener('change', () => {
      </div>
      </div>`;
 
-    setTimeout(function () {
+    let image = new Image();
+    image.onload = function () {
       loader.hidden = true;
       divCatInfo.hidden = false;
-    }, 500);
+    };
+    image.src = data[0].url;
   });
 });
+
+function fault() {
+  breedSelect.hidden = true;
+  loader.hidden = true;
+  error.hidden = false;
+}
